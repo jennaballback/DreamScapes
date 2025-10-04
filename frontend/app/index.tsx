f4fe63dd485392917d54c1796a8aedcfe365ce8e
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-10-04 18:45:58 -0400

    delete the default app after struggling to hook up nativewind to it. Got Nativewind hooked up on the blank canvas, a little nervous!

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,16 +1,10 @@
 import { Text, View } from "react-native";
-import './global.css'
+import "./global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-rose-500">
+      <Text>Home</Text>
     </View>
   );
 }
